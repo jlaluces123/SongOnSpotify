9e05faf909b11aeffea669db334306e5ebb6f8a5
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-10-01 13:14:59 -0400

    Basic Setup for services and app routes: complete

---
+++ backend/api/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const AuthServices = require('../../services/auth');
+const config = require('../../config');
+
+const router = express.Router();
+
+router.get('/login', async (req, res, next) => {
+    console.log('Logging In...');
+
+    try {
+        let authService = AuthServices(config.spotifyClientID);
+        await authService.authorize();
+        console.log('Authorize Endpoint Completed');
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+});
+
+module.exports = router;
