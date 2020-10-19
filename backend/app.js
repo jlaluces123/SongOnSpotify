650b1c80a0d5e9d40594342de6b03d7420a6a714
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-10-19 11:44:33 -0400

    Backend Callback Function: working on recieving token from Spotify now with BTOA

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { authRouter } = require('./api/routes/index');
 const app = express();
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 const config = require('../config');
 
+const uri = 'http://localhost:3377/api/auth/callback';
+const encodedURI = encodeURIComponent(uri);
+
 module.exports = {
     authorize: async (req, res) => {
-        const uri = 'http://localhost:3377/api/auth/callback';
-        const encodedURI = encodeURIComponent(uri);
-
         console.log('axios call....');
 
         axios
@@ -13,7 +13,6 @@ module.exports = {
                 `https://accounts.spotify.com/authorize?client_id=${config.spotifyClientID}&response_type=code&redirect_uri=${encodedURI}`
             )
             .then((response) => {
-                console.log(response.request.res.responseUrl);
                 res.status(200).json({ url: response.request.res.responseUrl });
             })
             .catch((err) => console.log('Authorize Error: ', err));
@@ -21,5 +20,32 @@ module.exports = {
 
     callback: async (req, res) => {
         console.log('callback reached');
+
+        console.log('Req.Query: ', req.query);
+
+        let body = {
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: encodedURI,
+            client_id: config.spotifyClientID,
+            client_secret: config.spotifyClientSecret,
+        };
+
+        axios
+            .post('https://accounts.spotify.com/api/token', {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Authorization:
+                        'Basic ' +
+                        btoa(
+                            config.spotifyClientID +
+                                ':' +
+                                config.spotifyClientSecret
+                        ),
+                },
+                body,
+            })
+            .then((response) => console.log('Token Response: ', response))
+            .catch((err) => console.log('Token Error: ', err));
     },
 };
