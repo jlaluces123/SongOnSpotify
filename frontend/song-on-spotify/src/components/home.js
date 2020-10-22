cfd7f0dbfb8c3f61840e5ffb262658d9aaac733e
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-10-22 11:03:51 -0400

    Frontend: small changes, moving onto getting user data

--- frontend/song-on-spotify/src/components/home.js
+++ frontend/song-on-spotify/src/components/home.js
@@ -1,27 +1,27 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
-
     useEffect(() => {
         console.log('Component Did Mount');
     });
 
     const loginSpotify = () => {
-        fetch('http://localhost:3377/api/auth/login')  
-            .then(res => res.json())
-            .then(data => {
-                window.location.href = data.url
-            })
-            .catch(err => console.log(err))   
-    }
+        console.log('SANITY check');
+        fetch('http://localhost:3377/api/auth/login')
+            .then((response) => response.json())
+            .then((data) => (window.location.href = data.url))
+            .catch((err) => console.log(err));
+
+        console.log('SANITY check END');
+    };
 
     return (
-        <div className="home">
+        <div className='home'>
             <h1>Welcome to Song On Spotify</h1>
 
             <button onClick={loginSpotify}>Login</button>
         </div>
     );
-}
- 
+};
+
 export default Home;
