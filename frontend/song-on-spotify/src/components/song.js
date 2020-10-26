443485aa515c4036bedac8f97b725dc467874d33
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-10-26 11:24:26 -0400

    Song Images: added dynamically sized image set for browser to use

--- frontend/song-on-spotify/src/components/song.js
+++ frontend/song-on-spotify/src/components/song.js
@@ -5,6 +5,17 @@ const Song = ({ artists, name, images, addSong }) => {
         <div className='song-container'>
             <div className='track-data'>
                 {/* Picture and Data goes here */}
+                <img
+                    src={images[1].url}
+                    srcSet={`
+                        ${images[1].url} ${images[1].width}w,
+                        ${images[2].url} ${images[2].width}w
+                    `}
+                    sizes={`
+                        (max-width: 600px) ${images[2].width}px, ${images[1].width}px
+                    `}
+                    alt='Track Art'
+                />
 
                 <div className='track-text'>
                     <h4>{name}</h4>
