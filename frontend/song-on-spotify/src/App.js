01ea819272f061f80b96aad0db7030e71d03cb19
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-10-29 14:50:11 -0400

    Tailwind CSS + PostCSS: added both npm packages to utilize tailwindcss

--- frontend/song-on-spotify/src/App.js
+++ frontend/song-on-spotify/src/App.js
@@ -6,14 +6,14 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home';
 
 class App extends React.Component {
-
     render() {
         return (
             <div className='App'>
+                <header>{/* Navigation */}</header>
 
-                <main className="content">
+                <main className='content'>
                     <Switch>
-                        <Route exact path = '/' component = {Home} />
+                        <Route exact path='/' component={Home} />
                     </Switch>
                 </main>
             </div>
