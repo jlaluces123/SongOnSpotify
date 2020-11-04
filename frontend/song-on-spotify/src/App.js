b6c4c1f57a42a91464e4b126ceb650f8a759fcdf
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-11-04 11:26:44 -0500

    Navigation Dynamic Functionality: now shows sign out button only if the user is logged in

--- frontend/song-on-spotify/src/App.js
+++ frontend/song-on-spotify/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/home';
 import Navigation from './components/navigation';
 
 const App = () => {
+    const [loggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLoggedIn = () => {
+        if (window.location.search) {
+            console.log('Is Logged In');
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    };
+
+    useEffect(() => {
+        console.log('Checking if user is logged in');
+        handleLoggedIn();
+    }, []);
+
     return (
         <div className='App'>
             <header>
                 {/* Navigation */}
-                <Navigation />
+                <Navigation loggedIn={loggedIn} />
             </header>
 
             <main className='content'>
--- frontend/song-on-spotify/src/components/navigation.js
+++ frontend/song-on-spotify/src/components/navigation.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
-const Navigation = () => {
+const Navigation = ({ loggedIn }) => {
     return (
         <div className='w-full h-16 px-6 flex justify-between items-center border-b-2 border-gray-300'>
             <h1 className='font-semibold cursor-pointer'>Song On Spotify</h1>
-            <a
-                className='text-sm text-gray-600 font-normal hover:text-black transition duration-300 ease-in-out'
-                href='/'
-            >
-                Sign Out
-            </a>
+
+            {loggedIn === true ? (
+                <a
+                    className='text-sm text-gray-600 font-normal hover:text-black transition duration-300 ease-in-out'
+                    href='/'
+                >
+                    Sign Out
+                </a>
+            ) : (
+                <span />
+            )}
         </div>
     );
 };
