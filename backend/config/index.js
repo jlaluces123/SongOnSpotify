07c04d1e16e3d92bb2a2e61a49fc120fba965062
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-11-05 11:06:19 -0500

    DotEnv issues: seeing if this works

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,8 +1,8 @@
-let dotenv = require('dotenv');
-
-const envFound = dotenv.config();
-
-if (envFound.error) throw new Error('Could not find .env file');
+if (process.env.NODE_ENV !== 'production') {
+    let dotenv = require('dotenv');
+    const envFound = dotenv.config();
+    if (envFound.error) throw new Error('Could not find .env file');
+}
 
 module.exports = {
     port: process.env.PORT || 3388,
