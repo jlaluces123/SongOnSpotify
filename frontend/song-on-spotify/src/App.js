759c45432889dc153f9db1694dbd1482e72e64f0
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-12-01 09:56:19 -0500

    Modal Setup: setup initial modal component and added a route to test it

--- frontend/song-on-spotify/src/App.js
+++ frontend/song-on-spotify/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/home';
+import Modal from './components/modal';
 import Navigation from './components/navigation';
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
             <main className='h-auto content flex-grow flex-shrink-0'>
                 <Switch>
                     <Route exact path='/' component={Home} />
+                    <Route path='/modal' component={Modal} />
                 </Switch>
             </main>
         </div>
