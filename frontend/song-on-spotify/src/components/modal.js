fa9365c6958525bbd5e9073a51973cc219fe9baa
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-12-01 12:21:05 -0500

    Modal Wiring: setting up modal to be opened, closed, and wiring up functionality to submit

--- frontend/song-on-spotify/src/components/modal.js
+++ frontend/song-on-spotify/src/components/modal.js
@@ -16,61 +16,88 @@ const Modal = () => {
         collaborative,
     ]);
 
-    return (
-        <div className='fixed z-10 inset-0 overflow-y-auto'>
-            <div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-                <div
-                    className='fixed inset-0 transition-opacity'
-                    ariaHidden='true'
-                >
-                    <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
-                </div>
+    const createPlaylist = (e) => {
+        e.preventDefault();
+        fetch('https://api.spotify.com/v1/users/{user_id}/playlists', {
+            method: 'POST',
+            body: {
+                name: name,
+                public: isPublic,
+                collaborative: collaborative,
+                description: description,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => console.log('Successfully Made Playlist: ', data))
+            .catch((err) =>
+                console.log('ERROR POST /v1/users/user_id/playlists', err)
+            );
+    };
 
-                <span
-                    className='hidden sm:inline-block sm:align-middle sm:h-screen'
-                    ariaHidden='true'
-                >
-                    &#8203;
-                </span>
+    if (open === true) {
+        return (
+            <div className='fixed z-10 inset-0 overflow-y-auto'>
+                <div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+                    <div
+                        className='fixed inset-0 transition-opacity'
+                        aria-hidden='true'
+                    >
+                        <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+                    </div>
 
-                <div
-                    className='p-6 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
-                    role='dialog'
-                    ariaModal='true'
-                    ariaLabelledby='modal-headline'
-                >
-                    <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-                        <div className='flex flex-col justify-between'>
-                            <h2 className='mb-4 text-2xl font-bold'>
-                                Create A Playlist
-                            </h2>
+                    <span
+                        className='hidden sm:inline-block sm:align-middle sm:h-screen'
+                        aria-hidden='true'
+                    >
+                        &#8203;
+                    </span>
 
-                            <div className='flex flex-col'>
-                                <label
-                                    for='name'
-                                    className='block text-sm font-medium text-gray-700 mb-1'
-                                >
-                                    Playlist Name
-                                </label>
-                                <input
-                                    autoComplete='off'
-                                    onChange={(e) => setName(e.target.value)}
-                                    type='text'
-                                    name='name'
-                                    id='name'
-                                    className='border border-transparent bg-gray-300 text-gray-600 focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm rounded-md'
-                                    placeholder='Morning Drive'
-                                />
-                            </div>
+                    <div
+                        className='p-6 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
+                        role='dialog'
+                        aria-modal='true'
+                        aria-labelledby='modal-headline'
+                    >
+                        <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+                            <div className='flex flex-col justify-between'>
+                                <h2 className='mb-4 text-2xl font-bold'>
+                                    Create A Playlist
+                                </h2>
 
-                            <section className='mt-4 flex flex-row'>
-                                <div className='flex flex-col w-1/2'>
+                                <div className='flex flex-col'>
                                     <label
-                                        htmlFor='collab-switch'
-                                        className='block text-sm font-medium text-gray-700 mb-1'
+                                        htmlFor='name'
+                                        className='block text-md font-medium text-gray-700 mb-1'
                                     >
-                                        Collaborative
+                                        Playlist Name
                                     </label>
+                                    <input
+                                        autoComplete='off'
+                                        onChange={(e) =>
+                                            setName(e.target.value)
+                                        }
+                                        type='text'
+                                        name='name'
+                                        id='name'
+                                        className='border border-transparent bg-gray-300 text-gray-600 focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm rounded-md'
+                                        placeholder='Morning Drive'
+                                    />
+                                </div>
+
+                                <div className='my-4 flex flex-row w-full items-center justify-between'>
+                                    <div className='flex flex-col w-2/3'>
+                                        <label
+                                            htmlFor='collab-switch'
+                                            className='block text-md font-medium text-gray-700 mb-1'
+                                        >
+                                            Collaborative
+                                        </label>
+                                        <p className='text-sm text-gray-600'>
+                                            Allow other users to team up and add
+                                            to the playlist!
+                                        </p>
+                                    </div>
+
                                     <label
                                         id='collab-switch'
                                         className='switch'
@@ -85,13 +112,20 @@ const Modal = () => {
                                     </label>
                                 </div>
 
-                                <div className='flex flex-col w-1/2'>
-                                    <label
-                                        htmlFor='public-switch'
-                                        className='block text-sm font-medium text-gray-700 mb-1'
-                                    >
-                                        Make Public
-                                    </label>
+                                <div className='my-4 flex flex-row w-full items-center justify-between'>
+                                    <div className='flex flex-col w-2/3'>
+                                        <label
+                                            htmlFor='public-switch'
+                                            className='block text-md font-medium text-gray-700 mb-1'
+                                        >
+                                            Make Public
+                                        </label>
+                                        <p className='text-sm text-gray-600'>
+                                            Allow other users to search, save,
+                                            and play your playlist!
+                                        </p>
+                                    </div>
+
                                     <label
                                         id='public-switch'
                                         className='switch'
@@ -105,28 +139,50 @@ const Modal = () => {
                                         <span className='slider round'></span>
                                     </label>
                                 </div>
-                            </section>
-                        </div>
-                    </div>
 
-                    <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
-                        <button
-                            type='button'
-                            className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-black text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
-                        >
-                            Create Playlist
-                        </button>
-                        <button
-                            type='button'
-                            className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
-                        >
-                            Cancel
-                        </button>
+                                <div className='flex flex-col mt-2'>
+                                    <label
+                                        htmlFor='description'
+                                        className='block text-md font-medium text-gray-700 mb-1'
+                                    >
+                                        Describe Your Playlist
+                                    </label>
+                                    <textarea
+                                        placeholder='For my morning commute to work!'
+                                        name='description'
+                                        id='description'
+                                        className='resize rounded-md border border-transparent bg-gray-300 text-gray-600'
+                                        onChange={(e) =>
+                                            setDescription(e.target.value)
+                                        }
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+                            <button
+                                onClick={(e) => createPlaylist(e)}
+                                type='button'
+                                className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-black text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
+                            >
+                                Create Playlist
+                            </button>
+                            <button
+                                onClick={(e) => setOpen(false)}
+                                type='button'
+                                className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
+                            >
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return <button onClick={(e) => setOpen(true)}>Open Modal</button>;
+    }
 };
 
 export default Modal;
